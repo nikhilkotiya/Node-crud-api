ce8616e087bf3d3ce4c032c11ad3c2ea6cd82d67
nikhilkotiya8 <nikhilkotiya8@gmail.com>
2022-05-11 03:14:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+const cors=require("cors");
+dotenv.config();
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useUnifiedTopology : true,useNewUrlParser:true },
+    ()=> console.log("connected to db")
+);  
+// import routes
+const productRoutes =  require("./routes/product"); 
+const req = require("express/lib/request");
+app.use(express.json());
+app.use(cors());
+// route Middlewares
+app.use("/api/products",productRoutes);
+
+app.listen(4000,() => console.log("server is running on port 4000"));
